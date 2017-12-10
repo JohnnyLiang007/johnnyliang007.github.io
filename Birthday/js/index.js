3f6026fdf4e9935bd871e6895016fbe4221a2bae
JohnnyLiang007 <1035885780@qq.com>
2017-12-10 18:20:57 +0800

    submit

--- Birthday/js/index.js
+++ Birthday/js/index.js
@@ -16,6 +16,12 @@ $('#login-button').click(function (event) {
         $('.title').html(userName+' Happy Birthday!');
         $('.wrapper').addClass('form-success');
         setTimeout(function(){location.href="HappyBirthday-1.html";},2000);
+    }else if(userName=="彤彤" &&  pwd=="HappyBirthday"){
+        event.preventDefault();
+        $('form').fadeOut(500);
+        $('.title').html(userName+' Happy Birthday!');
+        $('.wrapper').addClass('form-success');
+        setTimeout(function(){location.href="HappyBirthday-1.html";},2000);
     }
     else{
         alert("Wrong Password");
